9de2a7c799620548fcad49bb69a7a16245cd950a
M_mon <a84207248@vip.qq.com>
2019-11-07 15:01:43 +0800

    单例模式、原型模式

---
+++ untitled/src/design_pattern/singleton/HungrySingleton.java
@@ -0,0 +1,17 @@
+package design_pattern.singleton;
+
+/**
+ * @Description 饿汉式单例在类创建的同时就已经创建好一个静态的对象供系统使用，以后不再改变，所以是线程安全的，可以直接用于多线程而不会出现问题。
+ * @auther hasee
+ * @create 2019-11-07 10:22
+ */
+public class HungrySingleton {
+
+    //类加载即进行实例化，static final  限制修改
+    private static final HungrySingleton instance=new HungrySingleton();
+    private HungrySingleton(){}
+    public static HungrySingleton getInstance()
+    {
+        return instance;
+    }
+}
---
+++ untitled/src/design_pattern/singleton/SingletonLazy.java
@@ -0,0 +1,51 @@
+package design_pattern.singleton;
+
+/**
+ * @Description
+ * @auther hasee
+ * @create 2019-11-07 10:25
+ */
+public class SingletonLazy
+{
+    public static void main(String[] args)
+    {
+        President zt1=President.getInstance();
+        zt1.getName();    //输出总统的名字
+        President zt2=President.getInstance();
+        zt2.getName();    //输出总统的名字
+        if(zt1==zt2)
+        {
+            System.out.println("他们是同一人！");
+        }
+        else
+        {
+            System.out.println("他们不是同一人！");
+        }
+    }
+}
+class President
+{
+    private static volatile President instance=null;    //保证instance在所有线程中同步
+    //private避免类在外部被实例化
+    private President()
+    {
+        System.out.println("产生一个总统！");
+    }
+    public static synchronized President getInstance()
+    {
+        //在getInstance方法上加同步
+        if(instance==null)
+        {
+            instance=new President();
+        }
+        else
+        {
+            System.out.println("已经有一个总统，不能产生新总统！");
+        }
+        return instance;
+    }
+    public void getName()
+    {
+        System.out.println("我是美国总统：特朗普。");
+    }
+}
